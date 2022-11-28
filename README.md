8733ad576bcf73a34778c5d6cc4546b569f5f3e4
tracij1286 <tracij50@gmail.com>
2022-11-27 20:07:23 -0500

    added loops for false and alerts for questions

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
+
+
+- The motivation behind this project was to put my knowledge to the test with Javascript. The task was to use the tools learned in class to create a functioning Password Generator. 
+- This project was built to build my skills on the basics of Javascript involving calling functions, variables, alerts and so much more, 
+- This web page solves the consumers problem by creating a password for them by just a few clicks. 
+- Throughout this project I learned the basics of Javascript. I learned how to call functions, variables, if statments, to create strings and to utilize the console log. 
+
+
+
+![alt text](images/screenshot.png)
+
+## Credits (Sited Pages Used)
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/null
+
+https://www.w3schools.com/js/js_variables.asp
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+https://www.youtube.com/watch?v=duNmhKgtcsI&t=274s
+
+## License
+
+N/A
+
+---
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const generateBtn = document.querySelector('#generate');
+//Dom element
+var generateBtn = document.querySelector("#generate");
 
-// Function for #pw input
+// Write password to the #password input
 function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector('#password');
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
@@ -13,33 +14,33 @@ generateBtn.addEventListener('click', writePassword);
 
 // Generate password with a min of 8 characters and max 128
 function generatePassword() {
-  let pwLength = parseInt(prompt('Enter a number between 8-128'), 10);
-  while (pwLength < 8 || pwLength > 128 || Number.isNaN(pwLength) || pwLength === null) {
+  var pwLength = parseInt(prompt('Enter a number between 8-128'), 10);
+  while (pwLength > 128 || pwLength < 8 || Number.isNaN(pwLength) || pwLength == null) {
     alert("That's not a valid number. Please enter a number from 8-128.");
     pwLength = prompt('Enter a number between 8-128');
   }
 
-  // Defining variables for upper, lower numbers and special characters
-  let confirmUpper = false;
-  let confirmLower = false;
-  let confirmNumber = false;
-  let confirmSpecial = false;
+  // Defining variables for upper, lower, numbers and special characters
+  var confirmUpper = false;
+  var confirmLower = false;
+  var confirmNumber = false;
+  var confirmSpecial = false;
 
-  // " If "answers are false, it'll loop back through to get atleast one true.
-  while (confirmUpper === false && confirmLower === false
-    && confirmNumber === false && confirmSpecial === false) {
+  // " If " answers are false, it'll loop back through to get atleast one true.
+  while (confirmUpper == false && confirmLower == false
+    && confirmNumber == false && confirmSpecial == false) {
     alert('Please select a type of character');
-    confirmUpper = confirm('Would you like to include upper case letters?');
-    confirmLower = confirm('Would you like to include lower case letters?');
-    confirmNumber = confirm('Would you like to include numbers?');
-    confirmSpecial = confirm('Would you like to include special characters?');
+    confirmUpper = confirm('Would you like to include upper case letters? select OK if Yes, Cancel if No?');
+    confirmLower = confirm('Would you like to include lower case letters? select OK if Yes, Cancel if No?');
+    confirmNumber = confirm('Would you like to include numbers? select OK if Yes, Cancel if No?');
+    confirmSpecial = confirm('Would you like to include special characters? select OK if Yes, Cancel if No?');
   }
 
   // Character Arrays
-  const upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  const lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  const numericChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-  const specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '|', '[', ']', ';', "'", ':', '<', '>', '?', '/'];
+  var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var numericChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '|', '[', ']', ';', "'", ':', '<', '>', '?', '/'];
 
   // Array created based on the answers
   const passwordPool = [];
@@ -64,8 +65,8 @@ function generatePassword() {
 
   // last array, random characters from the pool
   function pushChar() {
-    const randomPassword = [];
-    for (let i = 0; i < pwLength; i += 1) {
+    var randomPassword = [];
+    for (var i = 0; i < pwLength; i += 1) {
       const item = passwordPool[Math.floor(Math.random() * passwordPool.length)];
       randomPassword.push(item);
     }
@@ -74,23 +75,23 @@ function generatePassword() {
 
   // checking if all conditions were met to generate pw
   function checkPassword(password) {
-    const checkUpper = (upperCaseChar.some((ele) => password.includes(ele)));
-    const checkLower = (lowerCaseChar.some((ele) => password.includes(ele)));
-    const checkNumeric = (numericChar.some((ele) => password.includes(ele)));
-    const checkSpecial = (specialChar.some((ele) => password.includes(ele)));
+    var checkUpper = (upperCaseChar.some((ele) => password.includes(ele)));
+    var checkLower = (lowerCaseChar.some((ele) => password.includes(ele)));
+    var checkNumeric = (numericChar.some((ele) => password.includes(ele)));
+    var checkSpecial = (specialChar.some((ele) => password.includes(ele)));
 
     console.log(checkUpper);
     console.log(checkLower);
     console.log(checkNumeric);
     console.log(checkSpecial);
 
-    return checkUpper === confirmUpper
-      && checkLower === confirmLower
-      && checkNumeric === confirmSpecial
-      && checkSpecial === confirmNumber;
+    return checkUpper == confirmUpper
+      && checkLower == confirmLower
+      && checkNumeric == confirmSpecial
+      && checkSpecial == confirmNumber;
   }
 
-  let password = [];
+  var password = [];
   while (!checkPassword(password)) {
     password = pushChar();
   }
